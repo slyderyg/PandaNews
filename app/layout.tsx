f0fc5d7b89e2704ffddb319ec50923ecec2a565e
slyderyg <kprokofev88@icloud.com>
2023-11-28 21:46:53 +0300

    redux store created

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
-
+import { ReduxProvider } from '@/redux/provider'
 
 
 export const metadata: Metadata = {
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body><ReduxProvider>{children}</ReduxProvider></body>
     </html>
   )
 }
